5c83b715248336b67c4d3331884e0fea38cc8a31
Stefan van Delft <stefan.van.delft@bettyblocks.com>
2019-02-23 13:19:07 +0100

    add tests and separate files

---
+++ bathy_timer.py
@@ -0,0 +1,127 @@
+# Original code by Mehmet Ozturk 
+# Modified by Sebastiaan van Essen 07/2016
+# This code combines the time precision of the GNSS network and data from ALM sensors
+# and sends the data via Ethernet to a computer as a stand alone module.
+#from _overlapped import NULL
+#from test.support import temp_cwd
+
+import os
+import sys
+import serial
+import threading
+import datetime
+import time
+import logging
+import socket
+import string
+
+#import gpio
+
+#/////////////////////////////////  Defining variables used for the data splitting    ///////////////////
+date = ''
+status = ',st'
+dataToSend = '$SBDAML,,,,,,,,ST' + '\r\n'
+dateNow = ''
+timeNow = ''
+dateTime = '' 
+setTime = '' 
+
+# IP adresses and ports for Ethernet transfers
+UDP_IP1 = "10.68.5.91"      
+#UDP_IP1 = "172.16.10.50"
+UDP_PORT1 = 5001
+#UDP_IP2 = "10.68.5.92"      
+UDP_IP2 = "172.16.10.50"
+UDP_PORT2 = 5001
+
+# naming the sockets for UDP communication
+sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#/////////////////////////////////   Defining triggers for functions    /////////////////////////////////
+# trigger is used for the PPS input. ZDA is used to monitor the time that has passed since the requested time(?) [original line: This trigger is to keep track of the "freshness" of the ZDA time info]
+# AML trigger is to see if there is unsend AML info
+bTrigger = False
+bZdaOntvangen = False
+bAmlOntvangen = False
+
+
+#Open Com port of GPZDA (Connected through P9_26)
+# Linking serZDA to the correct Com port with the correct baudrate and setting the state of the port to open.
+serZda = serial.Serial('/dev/ttyO1')
+serZda.baudrate = 19200
+serZda.isOpen()
+
+#Open Com port of AML (connected through P9_21 and P9_22)
+# Linking serAml to the correct Com port with the correct baudrate and setting the state of the port to open.
+serAml = serial.Serial('/dev/ttyO2')
+serAml.baudrate = 38400
+serAml.isOpen()
+
+def getTime():
+    # currentDateTime is the current time plus one second 
+    currentDateTimeRaw = datetime.datetime.now() + datetime.timedelta(seconds =1)
+    currentDateTime = currentDateTimeRaw.strftime('%H:%M:%S.%f,%d,%m,%Y')
+    currentTime = currentDateTime.split(',')
+    # format is day+month+year
+    global dateNow; dateNow = currentTime[1] + '-' + currentTime[2] + '-' + currentTime[3] + ',' + currentTime[0]
+
+
+#////////////////////////////////////// Serial Write loops  /////////////////////////////////////////////
+def writeCom1(textToWrite):                                                 # Serial port 1 ZDA Writer
+    serZda.write(textToWrite.encode(encoding='utf_8', errors='strict'))     # Encode data to serial protocol for Com1
+
+def writeCom2(textToWrite):                                                 # Serial poort 2 AML Writer
+    serAml.write(textToWrite.encode(encoding='utf_8', errors='strict'))     # Encode data to serial protocol for Com2
+
+
+#///////////////////////////////// This is what happenes when pin 7 (PPS) goes high   ///////////////////
+def pulse(channel):
+    global bZdaOntvangen
+    global dateTime
+    global status
+    print('pulse bench test')
+
+    #checking if the data has been received and setting the system time to the received date.
+    # After setting the time the statement gets reset to False for checking in the next cycle. status is also 
+    if bZdaOntvangen is True:
+        # Sets the system time to dateTime (the time set per ZDA)
+        os.system('date -s %s' % dateTime)
+        # status is set to ok as all seems ok
+        status = ",OK"
+       # bZdaOntvangen = False
+    else:
+        status = ',NZ'
+        bZdaOntvangen = False
+
+
+#////////////////////////////////////// Ethernet write loops   //////////////////////////////////////////
+def UDPsender():
+    global UDP_IP1
+    global UDP_PORT1
+    global UDP_IP2
+    global UDP_PORT2
+
+    while True:
+        global dataToSend
+        # send the string to the first IP address over UDP
+        sock1.sendto(dataToSend, (UDP_IP1, UDP_PORT1))
+
+        # Send the string to the second IP adress over UDP
+        sock2.sendto(dataToSend, (UDP_IP2, UDP_PORT2))
+
+        getTime()
+        time.sleep(0.5)
+
+
+#Start thread Ethernet UDP
+thrUDP = threading.Thread(name='UDPsender', target=UDPsender) # Create a thread for serial communication(thrAML) 
+thrUDP.start()
+
+#Start thread serial 1 ZDA Reader
+thrZda = threading.Thread(name='serZdaReader', target=serZdaReader) # Create a thread for serial communication(thrZDA) 
+thrZda.start()
+
+#Start thread serial 2 AML Reader
+thrAml = threading.Thread(name='serAmlReader', target=serAmlReader) # Create a thread for serial communication(thrAML) 
+thrAml.start()
---
+++ zda.py
@@ -0,0 +1,68 @@
+#/////////////////////////////////   Serial receive loops   /////////////////////////////////////////////
+def serZdaReader():
+    while True:
+        bLine = serZda.readline()
+        try:
+            # decode it into usable data
+            sLine = bLine.decode(encoding='utf_8')
+        except:
+            sLine = "0"
+
+        # Requesting access to global variable named bZdaOntvangen
+        global bZdaOntvangen
+        # Requesting access to global variable named status
+        global status
+        # parse the raw data string into usable variables
+        dateTime = parseZda(sLine)
+
+        # if there is no usable data print "dateTime is none"
+        if dateTime is None:
+            print('dateTime is none:')
+            # Boolean bZdaOntvangen is set to False
+            bZdaOntvangen = False
+            # Change the status to IZ (Invalid ZDA)
+            status = ",IZ"
+        # If the data is usable 
+        else:
+                bZdaOntvangen = True
+
+
+#Splitting the ZDA data into 8 variables, then process it to time and date
+def parseZda(raw_message):
+    global bZdaOntvangen
+    global status
+
+    # if no data is sent stop the madness
+    # (Stefan: legendary comment by our lord sebas. We should keep this comment)
+    if raw_message is None:
+        return None
+
+    try:
+        # with split() each comma seperated piece of raw_message is written in array sLines.
+        sLines = raw_message.split(',')
+        # if the data contains less then 7 blocks
+        if len(sLines) < 7:
+            return None
+        if len(sLines[1]) < 9:
+            return None
+        realTime = zdaParseTime(sLines[1])
+        # if string 2, 3 or 4 is longer then 2 digits stop the data
+        if len(sLines[2]) < 2 or len(sLines[3]) < 2 or len(sLines[4]) < 2:
+            return None
+        date = zdaParseDate(sLines)
+        global dateTime; dateTime = "'" + date + ' ' + realTime +"'"
+        # Send confirmation + data (ZDA OK >> parsed data ) to console and Com1
+        return ' ZDA OK' + ' >> ' + dateTime
+
+    except Exception as e:
+        print ('Exception: ' + str(e))
+
+
+def zdaParseTime(tempTime):
+        #Time in format HH:MM:SS
+        return tempTime[:2] + ':' + tempTime[2:4] + ':' + tempTime[4:6] + '.' + tempTime[7:]
+
+def zdaParseDate(sLines):
+        #in order: year, month date
+        return sLines[4] + '-' + sLines[3] + '-' + sLines[2]
+
