b669404a941b366eaf9a298884b13008db763244
Jeroen <jeroen@seabed.nl>
2019-02-26 11:17:56 +0100

    f

--- gpio.py
+++ gpio.py
@@ -3,7 +3,7 @@ import timer
 
 # Configuring the general pins for input/output (GPIO)
 # setting Pin P9_42 as input, also a pull-down resistor is turned on internally
-def pulseTrigger
+def pulseTrigger():
     GPIO.setup("P9_42", GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # This is the detector that sees the pin goes high then starts the function pulse 
 # When the triggerpin goes high start function pulse()
